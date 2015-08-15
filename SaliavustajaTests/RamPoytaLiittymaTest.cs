00ea5ac2da93f0e122d466ecf9dbc450205b3e1c
Turo Nylund <turo.nylund@outlook.com>
2015-08-15 13:23:35 +0300

    Luotuo pöydille tietokantaliittymä, josta kaikki pöydät haetaan.

---
+++ SaliavustajaTests/RamPoytaLiittymaTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Saliavustaja;
+using System;
+using System.Collections.Generic;
+
+namespace SaliavustajaTests
+{
+    [TestFixture]
+    public class RamPoytaLiittymaTest
+    {
+        RamPoytaLiittyma tietokantaLiittyma = new RamPoytaLiittyma();
+
+        [SetUp]
+        public void TestienAlustus()
+        {
+            tietokantaLiittyma = new RamPoytaLiittyma();
+        }
+
+        [Test]
+        public void HaeKaikkiPoydat()
+        {
+            List<Poyta> kaikkiPoydat = tietokantaLiittyma.HaeKaikkiPoydat();
+            Assert.That(kaikkiPoydat, Is.InstanceOf<List<Poyta>>());
+            Assert.AreEqual(10, kaikkiPoydat.Count);
+        }
+        
+    }
+}
