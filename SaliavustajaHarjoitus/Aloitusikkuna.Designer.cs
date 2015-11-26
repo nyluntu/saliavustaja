0b5fd7fa039c69fbb5706d5810a7c578b8d65998
Turo Nylund <turo.nylund@outlook.com>
2015-11-26 18:50:59 +0200

    Harjoitustyo osa 4

---
+++ SaliavustajaHarjoitus/Aloitusikkuna.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace SaliavustajaHarjoitus
+{
+    partial class Aloitusikkuna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tiedostoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uusiTilausToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lopetaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tiedostoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(586, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tiedostoToolStripMenuItem
+            // 
+            this.tiedostoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uusiTilausToolStripMenuItem,
+            this.lopetaToolStripMenuItem});
+            this.tiedostoToolStripMenuItem.Name = "tiedostoToolStripMenuItem";
+            this.tiedostoToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.tiedostoToolStripMenuItem.Text = "Tiedosto";
+            // 
+            // uusiTilausToolStripMenuItem
+            // 
+            this.uusiTilausToolStripMenuItem.Name = "uusiTilausToolStripMenuItem";
+            this.uusiTilausToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.uusiTilausToolStripMenuItem.Text = "Uusi tilaus";
+            this.uusiTilausToolStripMenuItem.Click += new System.EventHandler(this.uusiTilausToolStripMenuItem_Click);
+            // 
+            // lopetaToolStripMenuItem
+            // 
+            this.lopetaToolStripMenuItem.Name = "lopetaToolStripMenuItem";
+            this.lopetaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.lopetaToolStripMenuItem.Text = "Lopeta";
+            this.lopetaToolStripMenuItem.Click += new System.EventHandler(this.lopetaToolStripMenuItem_Click);
+            // 
+            // Aloitusikkuna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 314);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Aloitusikkuna";
+            this.Text = "Aloitusikkuna";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tiedostoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uusiTilausToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lopetaToolStripMenuItem;
+    }
+}
+
---
+++ SaliavustajaHarjoitus/UusiTilausIkkuna.cs
@@ -0,0 +1,34 @@
+﻿using Saliavustaja.Entiteetit;
+using Saliavustaja.Rajapinnat;
+using Saliavustaja.TietokantaLiittymat;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace SaliavustajaHarjoitus
+{
+    public partial class UusiTilausIkkuna : Form
+    {
+        public UusiTilausIkkuna()
+        {
+            InitializeComponent();
+        }
+
+        private void UusiTilausIkkuna_Load(object sender, EventArgs e)
+        {
+            LisaaPoydatPudotusvalikkoon();
+        }
+
+        private void LisaaPoydatPudotusvalikkoon()
+        {
+            PoytaDb poytaDb = new InMemoryPoytaDb();
+
+            List<Poyta> poydat = poytaDb.HaeKaikki();
+
+            foreach (Poyta poyta in poydat)
+            {
+                comboBoxPoydat.Items.Add(poyta);
+            }
+        }
+    }
+}
