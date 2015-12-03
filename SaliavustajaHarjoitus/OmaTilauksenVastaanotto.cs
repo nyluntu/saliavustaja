004bd0121a9e5f2b426767bcf6e007a60f1b5cc9
Turo Nylund <turo.nylund@outlook.com>
2015-12-03 20:46:28 +0200

    Haroitustyon osa 23

--- SaliavustajaHarjoitus/OmaTilauksenVastaanotto.cs
+++ SaliavustajaHarjoitus/OmaTilauksenVastaanotto.cs
@@ -38,6 +38,22 @@ namespace SaliavustajaHarjoitus
                     throw new Exception("Tilaus ei sisällä tilausrivejä.");
                 }
 
+                if(tilaus.Asiakas == null)
+                {
+                    throw new Exception("Tilaus ei sisällä asiakasta");
+                }
+
+                if(tilaus.Poyta == null)
+                {
+                    throw new Exception("Pöytää ei ole valittu. Tilausta ei voida vahvistaa");
+                }
+
+                Poyta poyta = poytaTietokanta.Hae(tilaus.Poyta.Id);
+                if(poyta.OnkoVarattu())
+                {
+                    throw new Exception("Pöytä on jo varattu. Tilausta ei voida vahvistaa.");
+                }
+
                 tilaus.VahvistaTilaus();
                 tilausTietokanta.Uusi(tilaus);
                 poytaTietokanta.VaraaPoyta(tilaus.Poyta.Id);
