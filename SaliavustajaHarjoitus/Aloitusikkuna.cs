651bc6cb774b4a09c3438057ac9eaf9e137576e0
Turo Nylund <turo.nylund@outlook.com>
2020-01-25 18:53:39 +0200

    Poistettu turhat kansiot ja tiedostot

--- SaliavustajaHarjoitus/Aloitusikkuna.cs
+++ SaliavustajaHarjoitus/Aloitusikkuna.cs
@@ -3,12 +3,6 @@ using Saliavustaja.Rajapinnat;
 using Saliavustaja.TietokantaLiittymat;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SaliavustajaHarjoitus
